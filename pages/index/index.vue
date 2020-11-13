73c678b8bd8d736a45cef69967471fea1b65d087
邓俊 <dengjun@teetron.com.cn>
2020-11-13 10:17:00 +0800

    “初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="index_page">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				msg: "测试uview-ui框架"
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
